95c59d75fc31aff2d3497b644163b0cdcbf32650
Unitrilo <gatopepito@DESKTOP-MEMMQCH.localdomain>
2021-07-29 13:17:19 -0500

    updated models

--- FinalProject/ghMonitor/models.py
+++ FinalProject/ghMonitor/models.py
@@ -7,21 +7,21 @@ class User(AbstractUser):
 
 class Plant(models.Model):
     species = models.CharField(max_length=35)
-    startdate = models.DateField()
+    startdate = models.DateTimeField(auto_now_add=True)
     showonlist = models.BooleanField(default=True)
 
 
-#No auto dates, since the date will be simulated
+
 #To be filled with simulator or admin control panel
 class Height(models.Model):
     subject = models.ForeignKey(Plant, on_delete=models.CASCADE, related_name="weight")
     height = models.DecimalField(max_digits=6, decimal_places=3)
-    measuredate = models.DateField()
+    measuredate = models.DateTimeField(auto_now_add=True)
 
 class LeafColors(models.Model):
     subject = models.ForeignKey(Plant, on_delete=models.CASCADE)
     colorpattern = models.CharField(max_length=35)  #will be url
-    measuredate = models.DateField()
+    measuredate = models.DateTimeField(auto_now_add=True)
 
 class DiseaseList(models.Model):
     name = models.CharField(max_length=35)
@@ -30,17 +30,17 @@ class DiseaseList(models.Model):
 class Diseased(models.Model):
     subject = models.ForeignKey(Plant, on_delete=models.CASCADE, related_name="disease")
     disease = models.ForeignKey(DiseaseList, on_delete=models.CASCADE)
-    detectiondate = models.DateField()
+    detectiondate = models.DateTimeField(auto_now_add=True)
 
 #To be filled with user interface
 class Harvest(models.Model):
     subject = models.ForeignKey(Plant, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     netweight = models.DecimalField(max_digits=5, decimal_places=3)
-    date = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
 
 class PlantLog(models.Model):
     subject = models.ForeignKey(Plant, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     report = models.CharField(max_length=350)
-    date = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
